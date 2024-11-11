bd028c6c5d61cb654adf20a5012ab847e64717b6
RuairiMullally <123960178+RuairiMullally@users.noreply.github.com>
2024-11-11 20:21:04 +0000

    Task 3.2 Final

--- Assignment3_BinaryTrees/bst_skeleton_test.c
+++ Assignment3_BinaryTrees/bst_skeleton_test.c
@@ -6,12 +6,9 @@ int main(){
     char data[] = {'F','L','O','C','C','I','N','A','U','C','I','N','I','H','I','L','I','P','I','L','I','F','I','C','A','T','I','O','N'};
     //char data[] = {'D', 'B', 'A', 'C', 'F', 'E', 'G'};
     Tree_Node* tree = create_bst(data);
-
     tree_print_sorted(tree);
-
-    //Tree_Node* result = tree_search(tree, 'B');
-    //printf("True: %c ", result->data);
-
+    Tree_Node* result = tree_search(tree, 'N');
+    printf("True: %c ", result->data);
     tree_delete(tree);
     return 0;
 }
